08ac854bec23a93ae723c4fd7b08ff1543205d9b
loxt <emannuelloxt@hotmail.com>
2020-11-13 11:24:07 -0400

    fix: remove google maps api

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -62,7 +62,6 @@ export default function Header() {
           />
           <span onClick={() => getList(text)} className='fa fa-search fa-2x' />
         </div>
-        {/* https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference=CnRtAAAATLZNl354RwP_9UKbQ_5Psy40texXePv4oAlgP4qNEkdIrkyse7rPXYGd9D_Uj1rVsQdWT4oRz4QrYAJNpFX7rzqqMlZw2h2E2y5IKMUZ7ouD_SlcHxYq1yL4KbKUv3qtWgTK0A6QbGh87GB3sscrHRIQiG2RrmU_jF4tENr9wGS_YxoUSSDrYjWmrNfeEHSGSc3FyhNLlBU&key=AIzaSyAeEmKzkgtVZRQy5_9ZRD4cJCrT472ASYA*/}
       </div>
       {menuList && (
         <div className={styles.listContainer}>
